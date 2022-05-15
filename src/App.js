f8ef93fb862fe03e1f2bd9c45527998824a4ac17
Ufere Kalu <dekalusha@gmail.com>
2022-05-15 02:20:13 +0200

    modification

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React and AWS Amplify
         </a>
       </header>
     </div>
